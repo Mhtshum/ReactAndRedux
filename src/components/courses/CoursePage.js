02395cebf04ecceb3de2e8f7d9cdb2830b3b04d8
Mhtshum <mohtishamzubairgh@gmail.com>
2020-09-05 21:25:57 +0500

    issue on submit page getting postback

--- src/components/courses/CoursePage.js
+++ src/components/courses/CoursePage.js
@@ -2,24 +2,26 @@ import React from 'react';
 
 class CoursePage extends React.Component
 {
-  constructor(props){
-    super(props);
-    this.state = {
-      course : {
-        title : ''
-      }
-    };
-  }   
+  state = {
+    course : {
+      title : ''
+    }
+  };     
   
   handleChange = e => {
     const course = {...this.state.course, title : e.target.value};    
     this.setState({course}); 
     console.log(this.state.course);
   };
+   
+  handleSubmit = e => {
+    alert(e.target.value);
+  };  
   
   render(){
+    //onsbumit on form enable enter key also submit while on button only clicking on button
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add course</h3>
         <input type='text' onChange={this.handleChange} text={this.state.course.title} />
