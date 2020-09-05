992d2073614ecb6455056cdf44638ca9698609e5
Mhtshum <mohtishamzubairgh@gmail.com>
2020-09-05 20:14:11 +0500

    soln. 2- use bind inside constructor

--- src/components/courses/CoursePage.js
+++ src/components/courses/CoursePage.js
@@ -9,16 +9,9 @@ class CoursePage extends React.Component
         title : ''
       }
     };
+    this.handleChange = this.handleChange.bind(this);
   }   
   
-  /*
-  handleChange = (e) => {
-    const course = {...this.state.course, title : e.target.value};    
-    this.setState({course}); 
-    //console.log(this.state.course);
-  };
-  */
-  // below implementation has this context issue which is not enclosing current class instance onChange={this.handleChange.bind(this)}  if bind is removed
   handleChange(e){	  
     const course = {...this.state.course, title : e.target.value};    
     this.setState({course}); 
@@ -26,13 +19,11 @@ class CoursePage extends React.Component
   }
   
   render(){
-    //commented syntax is avoiding us state undefined object error and setting context correctly for current object but it this new fn. will be credated each time for render method
-    //<input type='text' onChange={this.handleChange.bind(this)} text={this.
     return (
       <form>
         <h2>Courses</h2>
         <h3>Add course</h3>
-        <input type='text' onChange={this.handleChange.bind(this)} text={this.state.course.title} />
+        <input type='text' onChange={this.handleChange} text={this.state.course.title} />
         <input type='button' value='Save'/>
       </form>);    
   }
