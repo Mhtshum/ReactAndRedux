6b18f4e9835c20ae8a494f209e54620492d028d5
Mhtshum <mohtishamzubairgh@gmail.com>
2020-09-05 20:06:41 +0500

    issue: created for this context

--- src/components/courses/CoursePage.js
+++ src/components/courses/CoursePage.js
@@ -11,11 +11,18 @@ class CoursePage extends React.Component
     };
   }   
   
+  /*
   handleChange = (e) => {
     const course = {...this.state.course, title : e.target.value};    
     this.setState({course}); 
     //console.log(this.state.course);
   };
+  */
+  // below implementation has this context issue which is not enclosing current class instance onChange={this.handleChange.bind(this)}  if bind is removed
+  handleChange(e){	  
+    const course = {...this.state.course, title : e.target.value};    
+    this.setState({course}); 
+  }
   
   render(){
     //commented syntax is avoiding us state undefined object error and setting context correctly for current object but it this new fn. will be credated each time for render method
@@ -24,7 +31,7 @@ class CoursePage extends React.Component
       <form>
         <h2>Courses</h2>
         <h3>Add course</h3>
-        <input type='text' onChange={this.handleChange.bind(this)} text={this.state.course.title} />
+        <input type='text' onChange={this.handleChange} text={this.state.course.title} />
         <input type='button' value='Save'/>
       </form>);    
   }
