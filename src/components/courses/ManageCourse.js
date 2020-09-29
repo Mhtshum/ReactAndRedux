0944715f29919dc2eaab4d6b5132d95456e2a918
Mhtshum <mohtishamzubairgh@gmail.com>
2020-09-29 12:54:23 +0500

    revert changes on these files, not finished with it yet

--- src/components/courses/ManageCourse.js
+++ src/components/courses/ManageCourse.js
@@ -8,7 +8,7 @@ import { newCourse } from '../../../tools/mockData';
 import Spinner from '../common/Spinner';
 import { toast } from 'react-toastify';
 
-export const ManageCourse = ({courses, authors, loadAuthors, loadCourses, saveCourse, history, ...props}) => { 
+const ManageCourse = ({courses, authors, loadAuthors, loadCourses, saveCourse, history, ...props}) => { 
   const [ course, setCourse ] = useState({...props.course});  
   const [ saving, setSaving ] = useState(false);    
   const [ errors, setErrors ] = useState({});  
