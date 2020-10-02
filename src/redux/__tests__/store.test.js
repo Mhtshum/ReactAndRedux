ceb13d4d963c269278ac86b33d2dda3f0c6f1ff5
Mhtshum <mohtishamzubairgh@gmail.com>
2020-10-02 11:13:25 +0500

    integration test for multiple actions from store

--- src/redux/__tests__/store.test.js
+++ src/redux/__tests__/store.test.js
@@ -16,4 +16,22 @@ describe(" integration test for store", () => {
     expect(store.getState().courses.length).toEqual(1);
     //expect(store.getState().courses[0]).toEqual(newCourse);
   });
+  
+  it("Triggering from actions for saving course",()=>{
+    //arragnge
+    const newCourse1 = { title : 'a' };
+    const newCourse2 = { title : 'b' };
+    const newCourse3 = { title : 'c' };
+    const store = createStore(rootReducer,initialData);         
+    
+    //act 
+    store.dispatch(saveCourseSuccess(newCourse1));
+    store.dispatch(saveCourseSuccess(newCourse2));
+    store.dispatch(saveCourseSuccess(newCourse3));
+    
+    //assert
+    expect(store.getState().courses.length).toEqual(3);
+    //expect(store.getState().courses[0]).toEqual(newCourse1);
+  });
+  
 });  
